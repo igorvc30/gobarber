0daf9fb79eaf29cb2b6d15aff827f91871d76165
Igor V Costa <igor.vc30@gmail.com>
2019-06-30 22:27:03 -0300

    feat: create appointment notification
    Once an appointment is created a notification is created too, it is using a schema at mongodb.

---
+++ src/app/schemas/Notification.js
@@ -0,0 +1,24 @@
+import mangoose from 'mongoose';
+
+const NotificationSchema = new mangoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mangoose.model('Notification', NotificationSchema);
